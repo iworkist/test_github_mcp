abb7bb2dc8e9a6c454140c1c5d143858963d0ba9
jaehong park <iworkist@gmail.com>
2025-04-06 18:43:57 +0900

    Add helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def hello():
+    """Simple function that returns a greeting message"""
+    return "Hello, GitHub MCP World!"
+
+def main():
+    """Main function to print the greeting"""
+    message = hello()
+    print(message)
+
+if __name__ == "__main__":
+    main()
